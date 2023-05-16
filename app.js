35b9a9fa654eadce7fe810e441a1c2b56f6dd861
vibhatsu08 <mistryvedant17@gmail.com>
2023-05-16 21:34:05 +0530

    Implemented the task tags feature that displays the tags of each and every task.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-function Task(description, completed){
+function Task(description, tags, completed){
     this.description = description;
+    this.tags = tags;
     this.completed = completed;
 }
 
@@ -7,8 +8,11 @@ let tasks = [];
 
 function createTask(){
     const taskDescription = prompt("Enter what the task is about:");
+    const tags = prompt("What tags do you want for your task, tags are separated by spaces: ").trim();
+    tagsArray = tags.split(/\s+/);
+
     if (taskDescription.length > 0) {
-        const newTask = new Task(taskDescription, false);
+        const newTask = new Task(taskDescription, tagsArray, false);
         tasks.push(newTask);
         saveTasksToLocalStorage();
     }
@@ -32,19 +36,38 @@ function displayTasks(){
         const taskElement = document.createElement("div");
         taskElement.classList.add("task");
 
-        const taskDesc = document.createElement("span");
-        taskDesc.textContent = task.description;
-        taskElement.appendChild(taskDesc);
+        const taskElementContent = document.createElement("div");
+        taskElementContent.classList.add("task-content");
 
+        const taskDesc = document.createElement("div");
+        taskDesc.textContent = task.description;
+
+        const taskTagsContent = document.createElement("div");
+        taskTagsContent.classList.add("task-tags-content");
+
+
+        task.tags.forEach((taskTagElement) => {
+            const taskTag = document.createElement("div");
+            taskTag.classList.add("task-tag");
+            taskTag.textContent = "#" + taskTagElement;
+            taskTagsContent.appendChild(taskTag);
+        });
+
+        
         const deleteButton = document.createElement("button");
         deleteButton.classList.add("delete-task");
         deleteButton.textContent = "delete";
+        
         deleteButton.addEventListener("click", (event) => {
             deleteTask(index);
         });
+        
+        taskElementContent.appendChild(taskDesc);
+        taskElementContent.appendChild(deleteButton);
 
-        taskElement.appendChild(deleteButton)
-        tasksContainer.appendChild(taskElement);
+        taskElement.appendChild(taskElementContent);
+        taskElement.appendChild(taskTagsContent);
+        tasksContainer.appendChild(taskElement)
     });
 }
 
